ce98647087834e58f59d5c9b062e87a9e7f7509b
Suchon Prasert <58396402+toey10112@users.noreply.github.com>
2021-09-13 18:28:44 +0700

    remove static attributes

--- src/main/java/edu/ncsu/csc326/coffeemaker/CoffeeMaker.java
+++ src/main/java/edu/ncsu/csc326/coffeemaker/CoffeeMaker.java
@@ -20,9 +20,9 @@ import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
  */
 public class CoffeeMaker {
 	/** Array of recipes in coffee maker */
-	private static RecipeBook recipeBook;
+	private  RecipeBook recipeBook;
 	/** Inventory of the coffee maker */
-    private static Inventory inventory;
+    private  Inventory inventory;
 	
     /**
      * Constructor for the coffee maker
@@ -32,7 +32,18 @@ public class CoffeeMaker {
 	    recipeBook = new RecipeBook();
 		inventory = new Inventory();
 	}
-	
+
+	/**
+	 * Overloaded Constructor for the coffee maker
+	 *
+	 * @param recipeBook, inventory
+	 */
+	public CoffeeMaker(RecipeBook recipeBook, Inventory inventory) {
+		this.recipeBook = recipeBook;
+		this.inventory = inventory;
+	}
+
+
 	/**
 	 * Returns true if the recipe is added to the
 	 * list of recipes in the CoffeeMaker and false
